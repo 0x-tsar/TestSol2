84fbabf6f97ba5b41c7a7a3763d5d88392135964
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2020-11-14 10:03:31 -0300

    changing helloworld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,6 +2,12 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract HelloWorld {
+  string olamundo = 'Olá mundo!!'
+
   constructor() public {
+  }
+
+  function get_msg()public view returns(string){
+    return olamundo;
   }
 }
